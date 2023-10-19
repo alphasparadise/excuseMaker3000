470d179387045d5a310b077c86fdfcdd1a6e1e48
Alpha Huynh <alpha.enterprise.corp@gmail.com>
2023-10-19 01:42:54 -0400

    feature/EM3000-App-layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { SearchPanelComponent } from './content/search-panel/search-panel.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'excuse-maker',
+    component: SearchPanelComponent
+  },
+  { path: '**', redirectTo: '/excuse-maker', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './content/app-layout/app-layout.component';
+import { SearchPanelComponent } from './content/search-panel/search-panel.component';
+import { HeaderComponent } from './content/header/header.component';
+import { MaterialsModule } from 'src/common/modules/materials/materials.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    SearchPanelComponent,
+    HeaderComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
